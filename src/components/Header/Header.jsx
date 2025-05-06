66ebad959140e3a6de830a19abf6253dddd1b1c9
NizarNaser <nizarnaser17@gmail.com>
2025-05-07 01:51:36 +0200

    first commit26

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,26 @@
-import "./Header.css"
-import header_img from '/header_img.webp'
+import "./Header.css";
+import header_img from '/header_img.webp';
 import { useTranslation } from 'react-i18next';
+
 function Header() {
   const { t } = useTranslation();
+
   return (
     <div className="header">
       <img
         src={header_img}
         alt={t('header_img')}
+        loading="lazy"
         decoding="async"
-        loading="eager"
-        width="100%"
-        height="auto"
+        style={{ aspectRatio: '3 / 1', width: '100%', height: 'auto' }}
       />
       <div className="header-contents">
-
         <h2>{t('Order-your-favourite-food-here')}</h2>
         <p>{t('Choose-from-a-diverse-menu')}</p>
         <button>{t('View-Menu')}</button>
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;

b97f8ec9f0bf060f58398d1565ff3fb45ba7a2b2
NizarNaser <nizarnaser17@gmail.com>
2025-05-06 21:38:38 +0200

    first commit7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import "./Header.css"
-import header_img from '/header_img.jpg'
+import header_img from '/header_img.webp'
 import { useTranslation } from 'react-i18next';
 function Header() {
   const { t} = useTranslation();
   return (
     <div className="header">
-      <img src={header_img} alt={t('header_img')} />
+      <img rel="preload" src={header_img} alt={t('header_img')} />
       <div className="header-contents">
 
         <h2>{t('Order-your-favourite-food-here')}</h2>
